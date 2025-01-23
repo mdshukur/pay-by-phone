67890fb64cba82494bbaf3e202ed7d5f5d213a36
mdshukur <90003137+mdshukur@users.noreply.github.com>
2025-01-23 15:18:06 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,44 @@
+// logo slider js
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+  },
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// scroll top js
+const scrollToTopButton = document.getElementById("scrollToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopButton.classList.add("show");
+  } else {
+    scrollToTopButton.classList.remove("show");
+  }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
